23ca9b5189a39f3035ca628dc789460b6862dcd7
Văn Huy Ngô <86543998+VHN1305@users.noreply.github.com>
2023-11-26 01:33:34 +0700

    save notebooks

--- __init__.py
+++
@@ -1,3 +0,0 @@
-DATA_PATH = "pizzacam\\pizzacam.json"
-DATA_PATH_JSON_ERROR_AFTER_CLEANING = "pizzacam\\pizza_error_list.json"
-DATA_PATH_CSV_ERROR_AFTER_LABELING = "pizzacam\\pizza_error_list_with_label.csv"
